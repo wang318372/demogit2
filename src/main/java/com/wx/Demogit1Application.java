66bb34117c5f58c9ac7693205097f45bdb85a980
wangx <wang318372@163.com>
2020-02-19 00:40:11 +0800

    项目经理第一次提交项目

---
+++ src/main/java/com/wx/Demogit1Application.java
@@ -0,0 +1,13 @@
+package com.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demogit1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demogit1Application.class, args);
+    }
+
+}
