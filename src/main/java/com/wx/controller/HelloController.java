f89fa970171a689c5c17cd5f612b29931f5e16dd
wangx <wang318372@163.com>
2020-02-19 20:07:02 +0800

    142

--- src/main/java/com/wx/controller/HelloController.java
+++ src/main/java/com/wx/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("info")
     public String info() {
-        return "wx:   HelloStringBoot Info HahaHa        141";
+        return "wx:   HelloStringBoot Info HahaHa        144";
     }
 }
